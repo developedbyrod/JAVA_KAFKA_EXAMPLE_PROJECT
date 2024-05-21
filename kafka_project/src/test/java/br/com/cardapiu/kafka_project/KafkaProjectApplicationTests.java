fadd47ffcca4db92fb3a378509405b00dcc41882
codeasrof <rar2023@icloud.com>
2024-05-21 17:49:59 -0300

    init

---
+++ kafka_project/src/test/java/br/com/cardapiu/kafka_project/KafkaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cardapiu.kafka_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
