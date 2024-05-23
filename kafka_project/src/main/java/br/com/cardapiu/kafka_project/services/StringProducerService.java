c2853b679397d62c8f76605e8f144ca4335c63c2
codeasrof <rar2023@icloud.com>
2024-05-23 01:45:06 -0300

    adding simple cb log function on producer

--- kafka_project/src/main/java/br/com/cardapiu/kafka_project/services/StringProducerService.java
+++ kafka_project/src/main/java/br/com/cardapiu/kafka_project/services/StringProducerService.java
@@ -1,10 +1,11 @@
 package br.com.cardapiu.kafka_project.services;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+@Log4j2
 @RequiredArgsConstructor
 @Service
 public class StringProducerService {
@@ -13,6 +14,13 @@ public class StringProducerService {
     private final KafkaTemplate<String, String> kafkaTemplate;
 
     public void sendMessage(String message){
-        kafkaTemplate.send("string-topic", message);
+        kafkaTemplate.send("string-topic", message).thenAccept(
+                success -> log.info("Mensagem enviada: " + message)
+                ).exceptionally(
+                error -> {
+                    log.error("Erro ao enviar mensagem: " + message);
+                    return null;
+                }
+        );
     }
 }
